63d597031cff3d5c0f69c957c5ff09f33c9944fa
handong <1969690108@qq.com>
2023-08-08 19:22:19 +0800

    CNRF

--- shell/sgcls_train_test.sh
+++ shell/sgcls_train_test.sh
@@ -2,6 +2,8 @@ export CUDA_VISIBLE_DEVICES="0"
 export num_gpu=1
 export model_config="e2e_relation_X_101_32_8_FPN_1x"
 export predictor="MotifPredictor"
+export train_batch=8
+export test_batch=4
 export glove_dir="/home/user/Glove"
 export pretrained_model_dir="/home/user/wight/pretrained_faster_rcnn"
 export output_dir="/home/user/outputs/${predictor}/sgcls-$(date "+%m-%d-%H:%M:%S")"
@@ -12,7 +14,7 @@ export test_final_model=True
     ../tools/relation_train_net.py --config-file ../configs/${model_config}.yaml \
     MODEL.ROI_RELATION_HEAD.USE_GT_BOX True MODEL.ROI_RELATION_HEAD.USE_GT_OBJECT_LABEL False \
     MODEL.ROI_RELATION_HEAD.PREDICTOR ${predictor} \
-    SOLVER.IMS_PER_BATCH 8  TEST.IMS_PER_BATCH 4 \
+    SOLVER.IMS_PER_BATCH ${train_batch}  TEST.IMS_PER_BATCH ${test_batch} \
     DTYPE float16 SOLVER.MAX_ITER 50000 SOLVER.VAL_PERIOD 2000 \
     SOLVER.CHECKPOINT_PERIOD 2000 \
     GLOVE_DIR ${glove_dir} \
